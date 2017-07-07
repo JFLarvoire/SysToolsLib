1019efdfb59ef4d0575aba624af4daa9ebd70145
JFLarvoire <jf.larvoire@free.fr>
2017-07-07 16:35:10 +0200

    Fixed warnings when included in a C source.

--- C/SysLib/HardDisk.h
+++ C/SysLib/HardDisk.h
@@ -17,6 +17,7 @@
 *    2002-07-05 JFL Restore the default packing size in the end.	      *
 *    2016-04-12 JFL Include MultiOS.h.					      *
 *    2016-04-22 JFL Renamed the MULTIOS library as SYSLIB.		      *
+*    2017-07-06 JFL Fixed warnings when included in a C source.		      *
 *									      *
 *         © Copyright 2016 Hewlett Packard Enterprise Development LP          *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -50,32 +51,32 @@ typedef void * HANDLE;		/* Must be a near pointer */
 #ifndef STRUCT_PARTITION
 #define STRUCT_PARTITION
 
-typedef struct tagPARTITION	/* Partition table entry in the boot sector */
-{
-    BYTE boot; 			/* 0x80 = Bootable; 0x00 = not */
-    BYTE beg_head;		/* Beginning head number */
-    WORD beg_sect	:6;	/* Beginning sector number */
-    WORD beg_hcyl	:2;	/* Beginning cylinder number bits <9:8> */
-    WORD beg_lcyl	:8;	/* Beginning cylinder number bits <7:0> */
-    BYTE type; 			/* Type. 0 = None, 1 = DOS FAT 12 bits, etc...*/
-    BYTE end_head;		/* End head number */
-    WORD end_sect	:6;	/* End sector number */
-    WORD end_hcyl	:2;	/* End cylinder number bits <9:8> */
-    WORD end_lcyl	:8;	/* End cylinder number bits <7:0> */
-    DWORD first_sector; 	/* Index of first sector from drive sector 0 */
-    DWORD n_sectors;		/* Total number of sectors */
+#pragma warning(disable:4214)	/* Ignore the warning: nonstandard extension used : bit field types other than int */
+typedef struct tagPARTITION {	/* Partition table entry in the boot sector */
+  BYTE boot; 			/* 0x80 = Bootable; 0x00 = not */
+  BYTE beg_head;		/* Beginning head number */
+  WORD beg_sect	:6;		/* Beginning sector number */
+  WORD beg_hcyl	:2;		/* Beginning cylinder number bits <9:8> */
+  WORD beg_lcyl	:8;		/* Beginning cylinder number bits <7:0> */
+  BYTE type; 			/* Type. 0 = None, 1 = DOS FAT 12 bits, etc...*/
+  BYTE end_head;		/* End head number */
+  WORD end_sect	:6;		/* End sector number */
+  WORD end_hcyl	:2;		/* End cylinder number bits <9:8> */
+  WORD end_lcyl	:8;		/* End cylinder number bits <7:0> */
+  DWORD first_sector;	 	/* Index of first sector from drive sector 0 */
+  DWORD n_sectors;		/* Total number of sectors */
 } PARTITION;
+#pragma warning(default:4214)	/* Restore the warning: nonstandard extension used : bit field types other than int */
 
 #endif /* STRUCT_PARTITION */
 
 #ifndef STRUCT_MASTERBOOTSECTOR
 #define STRUCT_MASTERBOOTSECTOR
 
-typedef struct tagMASTERBOOTSECTOR	/* Boot sector structure */
-{
-    BYTE mbsCode[0x1BE];	/* Code and garbage up to offset 1BE */
-    PARTITION mbsPart[4];	/* 4 partition table entries */
-    WORD mbsSignature;		/* AA55 marks a valid boot sector */
+typedef struct tagMASTERBOOTSECTOR {	/* Boot sector structure */
+  BYTE mbsCode[0x1BE];		/* Code and garbage up to offset 1BE */
+  PARTITION mbsPart[4];		/* 4 partition table entries */
+  WORD mbsSignature;		/* AA55 marks a valid boot sector */
 } MASTERBOOTSECTOR;
 
 #endif /* STRUCT_MASTERBOOTSECTOR */
@@ -84,16 +85,15 @@ typedef struct tagMASTERBOOTSECTOR	/* Boot sector structure */
 
 /* Hard disk sector direct access */
 
-typedef struct tagHDGEOMETRY
-{
-    QWORD qwSectors;
-    WORD wSectorSize;
-    DWORD dwCyls;	/* number of physical cylinders on drive */
-    DWORD dwHeads;	/* number of physical heads on drive */
-    DWORD dwSects;	/* number of physical sectors per track */
-    DWORD dwXlatCyls;	/* number of translated cylinders on drive */
-    DWORD dwXlatHeads;	/* number of translated heads on drive */
-    DWORD dwXlatSects;	/* number of translated sectors per track */
+typedef struct tagHDGEOMETRY {
+  QWORD qwSectors;
+  WORD wSectorSize;
+  DWORD dwCyls;		/* number of physical cylinders on drive */
+  DWORD dwHeads;	/* number of physical heads on drive */
+  DWORD dwSects;	/* number of physical sectors per track */
+  DWORD dwXlatCyls;	/* number of translated cylinders on drive */
+  DWORD dwXlatHeads;	/* number of translated heads on drive */
+  DWORD dwXlatSects;	/* number of translated sectors per track */
 } HDGEOMETRY;
 
 /* OS-independant routines */
