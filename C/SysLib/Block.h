6ae7caa8b03fdd9ffe9a620279f6b347a39bafee
JFLarvoire <jf.larvoire@free.fr>
2017-08-03 12:54:03 +0200

    Added support for floppys and CDs.

--- C/SysLib/Block.h
+++ C/SysLib/Block.h
@@ -12,6 +12,8 @@
 *    2002-07-05 JFL Restore the default packing size in the end.	      *
 *    2016-04-12 JFL Include MultiOS.h.					      *
 *    2016-04-22 JFL Renamed the MULTIOS library as SYSLIB.		      *
+*    2017-07-15 JFL Added the floppy and CD disk types.   		      *
+*		    Removed BLOCK_TYPE_* constants. Use BT_* enums instead.   *
 *									      *
 *         © Copyright 2016 Hewlett Packard Enterprise Development LP          *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -43,16 +45,12 @@ typedef void * HANDLE;
 #define HandleToPtr(h) ((void *)(h))
 
 /* Block device access */
-typedef struct 
-    {
-    int iType;		/* 0=File; 1=Hard Disk; 2=Logical Volume. */
-    int iBlockSize;	/* Block size, in bytes. */
-    QWORD qwBlocks;	/* Total number of blocks in device */
-    HANDLE h;
-    } SBLOCKDEVICE;
-
-#define BLOCK_TYPE_FILE 0
-#define BLOCK_TYPE_HARDDISK 1
+typedef struct {
+  int iType;		/* 0=File; 1=Hard Disk; 2=Logical Volume; 3=Floppy; 4=CD. */
+  int iBlockSize;	/* Block size, in bytes. */
+  QWORD qwBlocks;	/* Total number of blocks in device */
+  HANDLE h;
+} SBLOCKDEVICE;
 
 HANDLE BlockOpen(char *pszName, char *pszMode);
 void BlockClose(HANDLE hDevice);
@@ -60,12 +58,13 @@ void BlockClose(HANDLE hDevice);
 QWORD BlockCount(HANDLE hDevice);
 int BlockSize(HANDLE hDevice);
 int BlockType(HANDLE hDevice);
-enum
-    {
-    BT_FILE,		/* 0 = File */
-    BT_HARDDISK,	/* 1 = Hard disk */
-    BT_LOGICALVOLUME,	/* 2 = Logical volume */
-    };
+enum {
+  BT_FILE,		/* 0 = File */
+  BT_HARDDISK,		/* 1 = Hard disk */
+  BT_LOGICALVOLUME,	/* 2 = Logical volume */
+  BT_FLOPPYDISK,	/* 3 = Floppy disk */
+  BT_COMPACTDISK,	/* 4 = CD/DVD/BlueRay */
+};
 int BlockRead(HANDLE hDevice, QWORD qwSector, WORD wNum, void FAR *pBuf);
 int BlockWrite(HANDLE hDevice, QWORD qwSector, WORD wNum, void FAR *pBuf);
 char *BlockIndexName(HANDLE hBlock);
