d944ea84c4066e143fb6fb75dc5cc612765c1368
JFLarvoire <jf.larvoire@free.fr>
2018-01-10 16:21:06 +0100

    Added workaround for warnings when running h2inc.exe.

--- C/LoDosLib/dosdrv.h
+++ C/LoDosLib/dosdrv.h
@@ -7,7 +7,8 @@
 *   Notes:								      *
 *									      *
 *   History:								      *
-*    1995/08/22 JFL Created this file					      *
+*    1995-08-22 JFL Created this file					      *
+*    2017-12-19 JFL Added workaround for warnings when running h2inc.exe.     *
 *									      *
 *      (c) Copyright 1995-2017 Hewlett Packard Enterprise Development LP      *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -16,14 +17,26 @@
 #define FALSE 0
 #define TRUE 1
 
-/* Define assembler register types for C */
-/* Note: H2INC outputs three HI4010 warnings: "identifier is a MASM keyword"
+/* Define assembler register types BYTE, WORD, DWORD for C */
+/* Note: H2INC.EXE outputs three HI4010 warnings: "identifier is a MASM keyword"
+	 when defining these with a typedef.
          I tried preventing that warning, using #ifndef _H2INC, or with info
          from https://msdn.microsoft.com/en-us/library/aa712931.aspx
-         or with pragmas, but found no solution that worked. */
+         or with pragmas, but found no solution that worked.
+         So instead, use macros _BYTE, _WORD, _DWORD, and define them
+         differently for C (using a typedef) and for H2INC (without that) */
+#ifndef _H2INC	/* Compiling in C or C++ programs */
 typedef unsigned char	BYTE;	/* 8 bits unsigned character */
 typedef unsigned short  WORD;   /* 16-bit unsigned value */
 typedef unsigned long   DWORD;  /* 32-bit unsigned integer */
+#define _BYTE  BYTE
+#define _WORD  WORD
+#define _DWORD DWORD
+#else		/* Converting to .inc with h2inc.exe */
+#define _BYTE unsigned char
+#define _WORD unsigned short
+#define _DWORD unsigned long
+#endif
 
 /*****************************************************************************/
 /*		     Definitions generic to all device drivers		     */
@@ -34,7 +47,7 @@ typedef unsigned long   DWORD;  /* 32-bit unsigned integer */
 typedef struct tagDeviceHeader		/* Device driver header */
     {
     struct tagDeviceHeader far *dhLink;     /* Link to the next driver */
-    WORD dhAttributes;			    /* Device attributes */
+    _WORD dhAttributes;			    /* Device attributes */
     void (near *dhStrategy)();		    /* Strategy-routine offset */
     void (near *dhInterrupt)(); 	    /* Interrupt-routine offset */
     char dhNameOrUnits[8];		    /* Logical device name */
@@ -42,22 +55,22 @@ typedef struct tagDeviceHeader		/* Device driver header */
 
 typedef struct		    /* Standard request header */
     {
-    BYTE rhLength;		/* Length in bytes of this request header */
-    BYTE rhUnit;		/* Subunit for block device drivers */
-    BYTE rhFunction;		/* Command code */
-    WORD rhStatus;		/* Status word returned by the driver */
-    BYTE rhReserved[8]; 	/* reserved */
+    _BYTE rhLength;		/* Length in bytes of this request header */
+    _BYTE rhUnit;		/* Subunit for block device drivers */
+    _BYTE rhFunction;		/* Command code */
+    _WORD rhStatus;		/* Status word returned by the driver */
+    _BYTE rhReserved[8]; 	/* reserved */
     } RequestHeader;
 
 typedef struct		    /* Input/output commands request */
     {
     RequestHeader h;
-    BYTE rwrMediaID;		/* Media descriptor from BPB */
+    _BYTE rwrMediaID;		/* Media descriptor from BPB */
     char far *rwrBuffer;	/* Transfer address */
-    WORD rwrBytesSec;		/* Bytes / sectors count */
-    WORD rwrStartSec;		/* Starting sector */
+    _WORD rwrBytesSec;		/* Bytes / sectors count */
+    _WORD rwrStartSec;		/* Starting sector */
     char far *rwrVolumeID;	/* Returned pointer on volume ID */
-    DWORD rwrHugeStartSec;	/* Starting sector if rwrStartSec=FFFF */
+    _DWORD rwrHugeStartSec;	/* Starting sector if rwrStartSec=FFFF */
     } ReadWriteRequest;
 
 typedef struct		    /* Non Destructive Read (Peek) command request */
