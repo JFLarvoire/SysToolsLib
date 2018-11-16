d8ae3cd9f2fdc09c3631f8c4bb7f1da3531ada5b
JFLarvoire <jf.larvoire@free.fr>
2018-11-16 14:01:17 +0100

    Added options -? for help, -A for ANSI character mode, -U for Unicode character mode, -V for the script version, -X for no-eXecute mode.

--- Batch/12.bat
+++ Batch/12.bat
@@ -21,24 +21,105 @@
 :#   2018-09-11 JFL Use codepage 65001 = UTF-8, to handle all Unicode chars   #
 :#                  in the pipe. This is possible now that flipmails.tcl      #
 :#                  correctly handles code pages, like my C tools did.        #
+:#   2018-10-18 JFL Added options -?, -A, -U, -V, -X.			      #
 :#                                                                            #
 :##############################################################################
 
-setlocal EnableExtensions EnableDelayedExpansion
-set "VERSION=2018-09-11"
+setlocal EnableExtensions DisableDelayedExpansion
+set "VERSION=2018-10-18"
+set ^"ARGS=%*^"					&:# Argument line
+goto :main
+
+:#----------------------------------------------------------------------------#
+
+:PopArg
+if "!!"=="" goto :PopArg.Eon
+:PopArg.Eoff
+:PopSimpleArg :# Will corrupt result if expansion is on and ARG contains ^ or ! characters.
+:# Gotcha: The call parser first scans its command line for an unquoted /?.
+:# If it finds one anywhere on the command line, then it ignores the target label and displays call help.
+:# To work around that, we initialize %ARG% and %"ARG"% with an impossible combination of values.
+set "ARG=Yes"
+set ""ARG"=No"
+set "PopArg.ARGS="
+if defined ARGS (
+  setlocal EnableDelayedExpansion
+  for /f "delims=" %%a in ("!ARGS:%%=%%%%!") do endlocal & set ^"PopArg.ARGS=%%a^"
+)
+call :PopArg.Helper %PopArg.ARGS% >NUL 2>NUL &:# Output redirections ensure the call help is not actually output.
+:# Finding that impossible combination now is proof that the call was not executed.
+:# In this case, try again with the /? quoted, to prevent the call parser from processing it.
+:# Note that we can not systematically do this /? enquoting, else it's "/?" that would break the call.
+if "%ARG%"=="Yes" if [%"ARG"%]==[No] call :PopArg.Helper %PopArg.ARGS:/?="/?"%
+set "PopArg.ARGS="
+goto :eof
+:PopArg.Helper
+set "ARG=%~1"		&:# Remove quotes from the argument
+set ^""ARG"=%1^"	&:# The same with quotes, if any, should we need them
+if defined ARG set "ARG=%ARG:^^=^%"
+if defined "ARG" set ^""ARG"=%"ARG":^^=^%^"
+:# Rebuild the tail of the argument line, as shift does not do it
+:# Never quote the set ARGS command, else some complex quoted strings break
+set ARGS=%2
+:PopArg.GetNext
+shift
+if defined ARGS set ^"ARGS=%ARGS:^^=^%^"
+if [%2]==[] goto :eof
+:# Leave quotes in the tail of the argument line
+set ARGS=%ARGS% %2
+goto :PopArg.GetNext
+
+:#----------------------------------------------------------------------------#
+
+:help
+echo Filter clipboard contents. Runs: 1clip.exe ^| FILTER [OPTIONS] ^| 2clip.exe
+echo.
+echo Usage:
+echo   12 [12_OPTIONS] FILTER [FILTER_OPTIONS]
+echo.
+echo 12 Options:
+echo   -?    Display this help and exit
+echo   -A    Filter ANSI characters. Default only for 82w.bat
+echo   -U    Filter UTF-8 characters. Default
+echo   -V    Display this script version and exit
+echo   -X    Display the commands to execute, but don't run them
+exit /b
+
+:#----------------------------------------------------------------------------#
+
+:main
+:# Get the default system code page
+:# set "KEY=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage"
+:# for /f "tokens=3" %%p in ('reg query "%KEY%" /v "ACP" ^| findstr REG_SZ') do set "ACP=%%p"
+:# set "NEWCP=%ACP%"
+:# Actually use UTF-8 in all cases now
+set "NEWCP=65001"
+set "_CLIPMODE="
+set "EXEC="
+set "|=|"
+set ">NUL=>NUL"
+
+:next_arg
+set ^"LASTARGS=%ARGS%^"
+call :PopArg
+if [%ARG%]==[-?] goto :help
+if [%ARG%]==[/?] goto :help
+if [%ARG%]==[-A] set "_CLIPMODE= -A" & goto next_arg
+if [%ARG%]==[-U] set "_CLIPMODE= -U" & goto next_arg
+if [%ARG%]==[-V] (echo.%VERSION%) & exit /b
+if [%ARG%]==[-X] set "EXEC=echo" & set "|=^|" & set ">NUL=" & goto next_arg
+
+if [%ARG%]==[82w] set "_CLIPMODE= -A"	  &:# Special case for the 82w.bat script
+if [%ARG%]==[82w.bat] set "_CLIPMODE= -A" &:# Special case for the 82w.bat script
+set ^"ARGS=%LASTARGS%^"
 
 :# Get the current console code page
 for /f "tokens=2 delims=:" %%n in ('chcp') do for %%p in (%%n) do set OLDCP=%%p
-:# Get the default system code page
-:# set "KEY=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage"
-:# for /f "tokens=3" %%p in ('reg query "%KEY%" /v "ACP" ^| findstr REG_SZ') do set "NEWCP=%%p"
-:# Actually use UTF-8 in all cases now
-set "NEWCP=65001"
 :# Change to the default system code page if needed
-if not "%OLDCP%"=="%NEWCP%" chcp %NEWCP% >NUL
+if not "%OLDCP%"=="%NEWCP%" %EXEC% chcp %NEWCP% %>NUL%
 
-1clip | %* | 2clip
+%EXEC% 1clip%_CLIPMODE% %|% %ARGS% %|% 2clip%_CLIPMODE%
 
 :# Return to the initial code page if it was different
-if not "%OLDCP%"=="%NEWCP%" chcp %OLDCP% >NUL
+if not "%OLDCP%"=="%NEWCP%" %EXEC% chcp %OLDCP% %>NUL%
 
