aaa478d6bf6d0649ac0b1dce13128fa10e3e8da6
JFLarvoire <jf.larvoire@free.fr>
2018-11-16 14:02:17 +0100

    Added the ability to search entries in the hosts file. Added a help screen.

--- Batch/hosts.bat
+++ Batch/hosts.bat
@@ -16,13 +16,15 @@
 :#		    Added option -X to test what command is executed.         #
 :#   2018-07-23 JFL Exit from the script without waiting for notepad to exit. #
 :#   2018-08-27 JFL Added -V option to display the script version.            #
+:#   2018-09-17 JFL Added the ability to search entries in the hosts file.    #
+:#		    Added a help screen.                                      #
 :#		                                                              #
 :#      © Copyright 2016-2018 Hewlett Packard Enterprise Development LP       #
 :# Licensed under the Apache 2.0 license  www.apache.org/licenses/LICENSE-2.0 #
 :##############################################################################
 
 setlocal EnableExtensions EnableDelayedExpansion
-set "VERSION=2018-08-27"
+set "VERSION=2018-09-17"
 goto :main
 
 :# runas /user:Administrator "%windir%\System32\notepad.exe" "%windir%\System32\Drivers\etc\hosts"
@@ -52,6 +54,29 @@ exit /b
 >NUL 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 goto :eof
 
+:help
+echo hosts file manager
+echo.
+echo Usage: hosts [OPTIONS] [NAMES]
+echo.
+echo Options:
+echo   -?    Display this help screen and exit
+echo   -V    Display the script version and exit
+echo   -X    Display the command to run, but don't run it
+echo.
+echo Names: List of names to search in the hosts file.
+echo        Default: Edit the hosts file.
+echo.
+echo Note:  When started from a non-Administrator Command Prompt, Notepad cannot
+echo        save the hosts file.
+echo        In this case, to prevent this problem, this script attemps to switch to
+echo        administrator mode. For that, it needs the free elevate.exe tool,
+echo        available at: http://www.winability.com/elevate
+echo        If you don't have elevate.exe in your PATH, or don't want to use it,
+echo        always run this script in an Administrator Command Prompt.
+exit /b
+
+:##############################################################################
 :# Main routine
 :main
 set "ELEVATE="
@@ -59,9 +84,28 @@ set "EXEC=start"
 set "NOTEPAD=%windir%\System32\notepad.exe"
 set "HOSTS=%windir%\System32\Drivers\etc\hosts"
 
-if [%1]==[-V] (echo %VERSION%) & exit /b &:# Display the script version and exit
-if [%1]==[-X] set "EXEC=call :echo"	 &:# Display the command, but don't run it
+goto :get_arg
+:next_arg
+shift
+:get_arg
+if [%1]==[] goto :edit_hosts
+if [%1]==[-V] (echo %VERSION%) & exit /b 		&:# Display the script version and exit
+if [%1]==[-X] set "EXEC=call :echo" & goto :next_arg	&:# Display the command, but don't run it
+if [%1]==[-?] goto :help				&:# Display the help screen and exit
+if [%1]==[/?] goto :help				&:# Display the help screen and exit
 
+:##############################################################################
+:# Search entries in the hosts file
+:show_ip
+if "%EXEC%"=="start" set "EXEC="
+%EXEC% findstr /R "\<%~1\>" %HOSTS%
+shift
+if not [%1]==[] goto :show_ip
+exit /b
+
+:##############################################################################
+:# Edit the hosts file
+:edit_hosts
 :# Check if we're running as administrator already
 call :IsAdmin
 if not errorlevel 1 goto :go &:# We are. Go run the notepad command directly.
