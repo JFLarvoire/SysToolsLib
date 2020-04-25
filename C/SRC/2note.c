adcf6b3f31139d8eca5412e5ddadd408ce758a5d
JFLarvoire <jf.larvoire@free.fr>
2020-04-25 17:35:23 +0200

    Rewrote ReportWin32Error as PrintWin32Error(); Added new PrintCError(), and use them instead of COMPLAIN(). Added option -z to stop input on Ctrl-Z. Added the -d debug option. Version 1.1.

--- C/SRC/2note.c
+++ C/SRC/2note.c
@@ -16,6 +16,10 @@
 *    2019-06-12 JFL Added PROGRAM_DESCRIPTION definition. Version 1.0.3.      *
 *    2019-09-27 JFL Fixed a formatting error in ReportWin32Error(), and       *
 *                   Prepend the program name to the output. Version 1.0.4.    *
+*    2020-04-25 JFL Added option -z to stop input on Ctrl-Z.                  *
+*                   Rewrote ReportWin32Error as PrintWin32Error(); Added new  *
+*		    PrintCError(), and use them instead of COMPLAIN().        *
+*		    Added the -d debug option. Version 1.1.		      *
 *		    							      *
 *         © Copyright 2018 Hewlett Packard Enterprise Development LP          *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -23,8 +27,8 @@
 
 #define PROGRAM_DESCRIPTION "Copy text from stdin to the Windows Notepad"
 #define PROGRAM_NAME    "2note"
-#define PROGRAM_VERSION "1.0.4"
-#define PROGRAM_DATE    "2019-09-27"
+#define PROGRAM_VERSION "1.1"
+#define PROGRAM_DATE    "2020-04-25"
 
 #define _UTF8_SOURCE	/* Tell MsvcLibX that this program generates UTF-8 output */
 
@@ -38,19 +42,10 @@
 #include <fcntl.h>
 #include <io.h>
 #include <windows.h>
+#include <errno.h>
 /* SysToolsLib include files */
 #include "debugm.h"	/* SysToolsLib debug macros */
 #include "stversion.h"	/* SysToolsLib version strings. Include last. */
-
-// Report an error.
-void COMPLAIN(char *s) {
-  DWORD e = GetLastError();
-  if (e) {
-    fprintf(stderr, "Error %uld: %s", e, s);
-  } else {
-    fprintf(stderr, "Error: %s", s);
-  }
-}
 
 // My favorite string comparison routines.
 #define streq(s1, s2) (!lstrcmp(s1, s2))     /* Test if strings are equal */
@@ -61,11 +56,16 @@ void COMPLAIN(char *s) {
 
 #define CP_NULL ((UINT)-1) /* No code page. Can't use 0 as CP_ACP is 0 */
 
+/* Global variables */
+
+DEBUG_GLOBALS
+
 /* Function prototypes */
 
 void usage(void);
 int IsSwitch(char *pszArg);
-int ToClip(const char* pBuffer, size_t lBuf, UINT cf);
+int _cdecl PrintCError(char *pszExplanation, ...);
+int _cdecl PrintWin32Error(char *pszExplanation, ...);
 int ToNotepadW(const WCHAR* pwBuffer, size_t nWChars);
 
 /*---------------------------------------------------------------------------*\
@@ -93,6 +93,8 @@ int main(int argc, char *argv[]) {
   int i;
   UINT codepage;
   int iDone;
+  int iRet;
+  int iCtrlZ = FALSE;		/* If true, stop input on a Ctrl-Z */
 
   /* Record the console code page, to allow converting the output accordingly */
   codepage = GetConsoleOutputCP();
@@ -109,6 +111,12 @@ int main(int argc, char *argv[]) {
 	codepage = CP_ACP;
 	continue;
       }
+      DEBUG_CODE(
+      if (streq(arg+1, "d")) {		/* -d: Debug */
+	iDebug = 1;
+	continue;
+      }
+      )
       if (streq(arg+1, "O")) {		/* Assume the input is OEM text */
 	codepage = CP_OEMCP;
 	continue;
@@ -125,13 +133,18 @@ int main(int argc, char *argv[]) {
 	puts(DETAILED_VERSION);
 	exit(0);
       }
-      fprintf(stderr, "Unsupported switch %s ignored.\n", arg);
+      if (streq(arg+1, "z")) {		/* -z: Stop input on Ctrl-Z */
+	iCtrlZ = TRUE;
+	continue;
+      }
+      fprintf(stderr, PROGRAM_NAME ".exe: Unsupported switch %s ignored.\n", arg);
       continue;
     }
-    fprintf(stderr, "Unexpected argument %s ignored.\n", arg);
+    fprintf(stderr, PROGRAM_NAME ".exe: Unexpected argument %s ignored.\n", arg);
   }
 
   /* Go for it */
+  DEBUG_PRINTF(("The selected code page is %d\n", codepage));
 
   /* Force stdin to untranslated */
   _setmode( _fileno( stdin ), _O_BINARY );
@@ -140,10 +153,22 @@ int main(int argc, char *argv[]) {
   while (!feof(stdin)) {
     pBuffer = realloc(pBuffer, nTotal + BLOCKSIZE);
     if (!pBuffer) {
-      COMPLAIN("Insufficient memory!");
+      PrintCError("Can't read all input");
       exit(1);
     }
-    nTotal += fread(pBuffer+nTotal, 1, BLOCKSIZE, stdin);
+    if (!iCtrlZ) {
+      nTotal += fread(pBuffer+nTotal, 1, BLOCKSIZE, stdin);
+    } else { /* Read characters 1 by 1, to avoid blocking if the EOF character is not on a BLOCKSIZE boundary */
+      int nRead;
+      for (nRead = 0; nRead < BLOCKSIZE; nRead++) {
+	char c;
+	if (!fread(&c, 1, 1, stdin)) break;
+	if (c == '\x1A') break; /* We got a SUB <==> EOF character */
+	pBuffer[nTotal+nRead] = c;
+      }
+      nTotal += nRead;
+      if (nRead < BLOCKSIZE) break;
+    }
     Sleep(0);	    // Release end of time-slice
   }
   if (nTotal > 0) {
@@ -152,7 +177,7 @@ int main(int argc, char *argv[]) {
     if (codepage != CP_NULL) {
       pwUnicodeBuf = (WCHAR *)malloc(2*(nTotal));
       if (!pwUnicodeBuf) {
-	COMPLAIN("Insufficient memory!");
+	PrintCError("Can't convert the input to Unicode");
 	exit(1);
       }
       nTotal = MultiByteToWideChar(codepage,		/* CodePage, (CP_ACP, CP_OEMCP, CP_UTF8, ...) */
@@ -163,22 +188,20 @@ int main(int argc, char *argv[]) {
 				   (int)nTotal		/* cchWideChar, */
 				   );
       if (!nTotal) {
-	COMPLAIN("Can't convert the intput to Unicode!");
+	PrintWin32Error("Can't convert the intput to Unicode");
 	exit(1);
       }
     } else {
       nTotal /= 2;	/* # of wide unicode characters */
     }
     iDone = ToNotepadW(pwUnicodeBuf, nTotal);
-    if (!iDone) {
-      COMPLAIN("Failed to write to Notepad!");
-      exit(1);
-    }
+    iRet = !iDone; /* Return exit code 0 is the copy was done; 1 if it was not */
   } else {
     /* This is actually not a bug: The input CAN be empty */
+    iRet = 0;
   }
 
-  return 0;
+  return iRet;
 }
 
 /*---------------------------------------------------------------------------*\
@@ -218,6 +241,7 @@ Switches:\n\
   -u        The input is UTF-16 text (Unicode)\n\
   -U        The input is UTF-8 text (Code page 65001)\n\
   -V        Display the program version\n\
+  -z        Stop input on a Ctrl-Z (aka. SUB or EOF) character\n\
 \n\
 Default input encoding: The current console code page (Code page %u).\n\
 \n"
@@ -230,27 +254,29 @@ Default input encoding: The current console code page (Code page %u).\n\
 
 /*---------------------------------------------------------------------------*\
 *                                                                             *
-|   Function:	    IsSwitch						      |
+|   Function	    IsSwitch						      |
 |									      |
-|   Description:    Test if a command line argument is a switch.	      |
+|   Description     Test if a command line argument is a switch.	      |
 |									      |
-|   Parameters:     char *pszArg					      |
+|   Parameters      char *pszArg					      |
 |									      |
-|   Returns:	    TRUE or FALSE					      |
+|   Returns	    TRUE or FALSE					      |
 |									      |
-|   Notes:								      |
+|   Notes								      |
 |									      |
-|   History:								      |
-|									      |
+|   History								      |
 |    1997-03-04 JFL Created this routine				      |
+|    2016-08-25 JFL "-" alone is NOT a switch.				      |
 *									      *
 \*---------------------------------------------------------------------------*/
 
 int IsSwitch(char *pszArg) {
   switch (*pszArg) {
     case '-':
+#if defined(_MSDOS) || defined(_WIN32)
     case '/':
-      return TRUE;
+#endif
+      return (*(short*)pszArg != (short)'-'); /* "-" is NOT a switch */
     default:
       return FALSE;
   }
@@ -258,37 +284,90 @@ int IsSwitch(char *pszArg) {
 
 /*---------------------------------------------------------------------------*\
 *                                                                             *
-|   Function:	    ReportWin32Error					      |
+|   Function	    PrintCError						      |
+|									      |
+|   Description     Print C library error messages with a consistent format   |
+|									      |
+|   Parameters      char *pszFormat	The error context, or NULL            |
+|		    ...			Optional arguments		      |
+|		    							      |
+|   Returns	    The number of characters written			      |
+|									      |
+|   Notes	    							      |
+|		    							      |
+|   History								      |
+|    2020-04-25 JFL Created this routine				      |
+*									      *
+\*---------------------------------------------------------------------------*/
+
+int _cdecl PrintCError(char *pszFormat, ...) {
+  va_list vl;
+  int n;
+  int e = errno; /* The initial C errno when this routine starts */
+
+  n = fprintf(stderr, PROGRAM_NAME
+#if defined(_MSDOS) || defined(_WIN32)
+		      ".exe"
+#endif
+		      ": ");
+
+  if (pszFormat) {
+    va_start(vl, pszFormat);
+    n += vfprintf(stderr, pszFormat, vl);
+    n += fprintf(stderr, ". ");
+    va_end(vl);
+  }
+
+  n += fprintf(stderr, "%s.\n", strerror(e));
+
+  return n;
+}
+
+/*---------------------------------------------------------------------------*\
+*                                                                             *
+|   Function:	    PrintWin32Error					      |
 |                                                                             |
 |   Description:    Display a message with the last error.		      |
 |                   							      |
-|   Parameters:     char *pszExplanation    Why we think this occured, or NULL|
-|                   ...			    Optional arguments to format      |
+|   Parameters      char *pszFormat	The error context, or NULL            |
+|		    ...			Optional arguments		      |
 |                   							      |
 |   Returns:	    The number of characters written.        		      |
 |                   							      |
-|   Notes:	    Danger: Known issue: We use a fixed 4KB buffer for the    |
-|		    error message, and do not check for overflows. This is    |
-|		    more than enough for all actual error message, but be     |
-|		    cautious anyway when using optional formatted arguments.  |
+|   Notes:	    							      |
 |                   							      |
 |   History:								      |
-|    1998-11-19 jfl Created this routine.				      |
+|    1998-11-19 JFL Created routine ReportWin32Error.			      |
 |    2005-06-10 JFL Added the message description, as formatted by the OS.    |
 |    2010-10-08 JFL Adapted to a console application, output to stderr.       |
 |    2018-11-02 JFL Allow pszExplanation to be NULL.			      |
 |                   Make sure WIN32 msg and explanation are on the same line. |
 |    2019-09-27 JFL Fixed a formatting error.                                 |
 |                   Prepend the program name to the output.		      |
+|    2020-04-25 JFL Redesigned to avoid using a fixed size buffer.	      |
+|                   Reordered the output so that it sounds more natural.      |
+|                   Renamed as PrintWin32Error.				      |
 *                                                                             *
 \*---------------------------------------------------------------------------*/
 
-int _cdecl ReportWin32Error(char *pszExplanation, ...) {
-  char szErrorMsg[4096];
-  va_list pArgs;
+int _cdecl PrintWin32Error(char *pszFormat, ...) {
+  va_list vl;
   int n;
-  LPVOID lpMsgBuf;
-  DWORD dwErr = GetLastError();
+  char FAR *lpMsgBuf;
+  DWORD dwErr = GetLastError(); /* The initial WIN32 error when this routine starts */
+
+  n = fprintf(stderr, PROGRAM_NAME
+#if defined(_MSDOS) || defined(_WIN32)
+		      ".exe"
+#endif
+		      ": ");
+
+  if (pszFormat) {
+    va_start(vl, pszFormat);
+    n += vfprintf(stderr, pszFormat, vl);
+    n += fprintf(stderr, ". ");
+    va_end(vl);
+  }
 
   if (FormatMessage( 
       FORMAT_MESSAGE_ALLOCATE_BUFFER | 
@@ -300,23 +379,18 @@ int _cdecl ReportWin32Error(char *pszExplanation, ...) {
       (LPTSTR) &lpMsgBuf,
       0,
       NULL )) { // Display both the error code and the description string.
-    n = wsprintf(szErrorMsg, "Error %lu: %s", dwErr, lpMsgBuf);
+    int l = lstrlen(lpMsgBuf);
+    // Remove the trailing new line and dot, if any.
+    if (l && (lpMsgBuf[l-1] == '\n')) lpMsgBuf[--l] = '\0';
+    if (l && (lpMsgBuf[l-1] == '\r')) lpMsgBuf[--l] = '\0';
+    if (l && (lpMsgBuf[l-1] == '.')) lpMsgBuf[--l] = '\0';
+    n += fprintf(stderr, "%s.\n", lpMsgBuf);
     LocalFree(lpMsgBuf); // Free the buffer.
-    // Remove the trailing new line
-    if (n && (szErrorMsg[n-1] == '\n')) szErrorMsg[--n] = '\0';
-    if (n && (szErrorMsg[n-1] == '\r')) szErrorMsg[--n] = '\0';
   } else { // Error, we did not find a description string for this error code.
-    n = wsprintf(szErrorMsg, "Error %lu.", dwErr);
+    n += fprintf(stderr, "Win32 error %lu.\n", dwErr);
   }
 
-  if (pszExplanation) {
-    szErrorMsg[n++] = ' '; // Add a blank separator
-    va_start(pArgs, pszExplanation);
-    n += wvsprintf(szErrorMsg+n, pszExplanation, pArgs);
-    va_end(pArgs);
-  }
-
-  return fprintf(stderr, PROGRAM_NAME ".exe: %s\n", szErrorMsg);
+  return n;
 }
 
 /*---------------------------------------------------------------------------*\
@@ -398,7 +472,7 @@ int ToNotepadW(const WCHAR* pwBuffer, size_t nWChars) {
   /* Allocate a global buffer with extra space to allow converting all \n to \r\n */
   pGlobalBuf = (WCHAR *)GlobalAlloc(GMEM_FIXED, (nWChars+nLF+1)*sizeof(WCHAR));
   if (!pGlobalBuf) {
-    ReportWin32Error("Can't allocate a global buffer.");
+    PrintWin32Error("Can't allocate a global buffer");
     return FALSE;
   }
   /* Copy the buffer, making sure everything's appears correctly in Notepad */
@@ -422,7 +496,7 @@ int ToNotepadW(const WCHAR* pwBuffer, size_t nWChars) {
 		     &si,     		// Pointer to STARTUPINFO structure
 		     &pi )    		// Pointer to PROCESS_INFORMATION structure
      ) {
-    ReportWin32Error("CreateProcess failed.");
+    PrintWin32Error("Can't start notepad.exe");
     goto cleanup;
   }
 
@@ -433,13 +507,13 @@ int ToNotepadW(const WCHAR* pwBuffer, size_t nWChars) {
     if (hMainWnd) break;
   }
   if (!hMainWnd) {
-    fprintf(stderr, "Failed to get Notepad main window handle.\n");
+    PrintWin32Error("Failed to get Notepad main window handle");
     goto cleanup;
   }
   /* Find Notepad content area window */
   hWnd = FindWindowEx(hMainWnd, 0, "Edit", NULL);
   if (!hWnd) {
-    fprintf(stderr, "Failed to get Notepad edit window handle.\n");
+    PrintWin32Error("Failed to get Notepad edit window handle");
     goto cleanup;
   }
 
