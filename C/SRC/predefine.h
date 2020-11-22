cebe7def23d5fca58a36898c3f6a701d6c3c3950
JFLarvoire <jf.larvoire@free.fr>
2020-11-22 22:52:49 +0100

    Added support for FreeBSD.

--- C/SRC/predefine.h
+++ C/SRC/predefine.h
@@ -13,6 +13,7 @@
 *   History								      *
 *    2020-03-19 JFL jf.larvoire@hpe.com created this file.                    *
 *    2020-04-19 JFL Added support for MacOS.                                  *
+*    2020-11-10 JFL Added support for FreeBSD.                                  *
 *		                                                              *
 *         © Copyright 2020 Hewlett Packard Enterprise Development LP          *
 * Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
@@ -23,6 +24,10 @@
 
 #define _CRT_SECURE_NO_WARNINGS 1 /* Disable Microsoft Visual C++ security warnings for old routines */
 
+/* XXXX_SOURCE macros, defining what flavor of Unix the program is written for */
+
+#ifndef __FreeBSD__		/* Contrary to the others, FreeBSD is substractive. It defines everything by default. */
+
 #define _POSIX_SOURCE		/* Define POSIX extensions. Ex: function fileno in stdio.h */
 #define _XOPEN_SOURCE		/* Define XOPEN extensions. Ex: function tempnam in stdio.h */
 #define _BSD_SOURCE    		/* Define BSD extensions. Ex: S_IFREG in sys/stat.h */
@@ -30,13 +35,19 @@
 #define _LARGEFILE_SOURCE	/* Define LFS extensions. Ex: type off_t, and functions fseeko and ftello */
 #define _LARGEFILE64_SOURCE	/* Define extra routines supporting 64-bit file sizes */
 #define _GNU_SOURCE		/* Implies all the above. And also MsvcLibX support for UTF-8 I/O */
-#define _FILE_OFFSET_BITS 64	/* Force using 64-bits file sizes by default, if the OS supports it */
 #define _LARGEFILE_SOURCE64	/* Old obsolete alias for the same? */
 
 #define _ISOC99_SOURCE		/* Tell the GNU library that we want C99 extensions */
-#define __STDC_LIMIT_MACROS	/* Make sure C99 macros are defined in C++ */
-#define __STDC_CONSTANT_MACROS
 
 #define _DARWIN_C_SOURCE    	/* On a Mac, define MacOS extensions */
+
+#endif  /* __FreeBSD__ */
+
+/* Other extension definition control macros */
+
+#define _FILE_OFFSET_BITS 64	/* Force using 64-bits file sizes by default, if the OS supports it */
+
+#define __STDC_LIMIT_MACROS	/* Make sure C99 macros are defined in C++ */
+#define __STDC_CONSTANT_MACROS
 
 #endif  /* _CLIBX_PREDEFINE_H_ */
