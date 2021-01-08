eee77d6b760f90f75466b3ab441d0941f876ee6e
JFLarvoire <jf.larvoire@free.fr>
2021-01-08 11:09:04 +0100

    Allow building multiple programs in a single command. Delete all local variables before running make.bat, which sometimes did confuse make.bat.

--- C/SRC/exe.bat
+++ C/SRC/exe.bat
@@ -18,71 +18,71 @@
 :#		    Added option -r for completeness.                         *
 :#   2015-11-13 JFL Adapted to the new multitarget make system.               *
 :#   2020-12-15 JFL Accept either program or program.exe as target arguments. *
-:#			                                                      *
+:#   2021-01-07 JFL Avoid unnecessary spaces in the make command generated.   *
+:#   2021-01-08 JFL Allow building multiple programs.                         *
+:#		    Delete all local variables before running make.bat.       *
+:#		    	                                                      *
 :#         © Copyright 2016 Hewlett Packard Enterprise Development LP         *
 :# Licensed under the Apache 2.0 license  www.apache.org/licenses/LICENSE-2.0 *
 :#*****************************************************************************
 
 setlocal enableextensions enabledelayedexpansion
-set "VERSION=2020-12-15"
+set "VERSION=2021-01-08"
 goto main
 
 :main
-set "FORCE=0"
-set "ACTION=default"
 set "EXEC="
-set "MAKEOPTS="
+set "REL=1"		&:# 1=Build release versions
+set "DBG=1"		&:# 1=Build debug versions
+set "MAKEOPTS="		&:# nmake.exe /options
+set "TARGETS="		&:# nmake targets to build
 
-goto get_arg
+goto :get_arg
 :next_arg
 shift
 :get_arg
-if .%1.==.-?. goto help
-if .%1.==./?. goto help
-if .%1.==.-a. set "ACTION=all" & goto next_arg
-if .%1.==.-d. set "ACTION=debug" & goto next_arg
-if .%1.==.-f. set "FORCE=1" & goto next_arg
-if .%1.==.-r. set "ACTION=release" & goto next_arg
+if [%1]==[] goto :start
+if .%1.==.-?. goto :help
+if .%1.==./?. goto :help
+if .%1.==.-a. set "REL=1" & set "DBG=1" & goto :next_arg
+if .%1.==.-d. set "REL=0" & set "DBG=1" & goto :next_arg
+if .%1.==.-f. set ^"MAKEOPTS=%MAKEOPTS% /A^" & goto :next_arg
+if .%1.==.-r. set "REL=1" & set "DBG=0" & goto :next_arg
 if .%1.==.-V. (echo.%VERSION%) & exit /b
 if .%1.==.-X. set "EXEC=echo" & goto next_arg
+set "ARG=%~1"
+if "%ARG:~0,1%%ARG:~0,1%"=="--" >&2 echo Unsupported option %1 & goto :next_arg
+if "%ARG:~0,1%%ARG:~0,1%"=="//" set ^"MAKEOPTS=%MAKEOPTS% %1^" & goto :next_arg &:# nmake /option
+for /f "tokens=2 delims==" %%v in ("%ARG%") do set ^"MAKEOPTS=%MAKEOPTS% %1^" & goto :next_arg &:# nmake macro=value
 set "PROGRAM=%~1.exe"
 set "PROGRAM=%PROGRAM:.exe.exe=.exe%"
-if "%PROGRAM%"==".exe" (>&2 echo Error: No target program specified) & exit /b 1
-set MAKEOPTS=%2 %3 %4 %5 %6 %7 %8 %9
-if "%FORCE%"=="1" set "MAKEOPTS=%MAKEOPTS% /A"
-goto %ACTION%
+if %REL%==1 set "TARGETS=%TARGETS% %PROGRAM%"
+if %DBG%==1 set "TARGETS=%TARGETS% debug\%PROGRAM%"
+goto :next_arg
 
 :help
 echo.
 echo..exe program builder from a C or C++ source
 echo.
-echo.Usage: exe [options] program [nmake_options]
+echo.Usage: exe [-options] [/nmake_options] [macro_defs] program [program ...]
 echo.
 echo.Options:
 echo.
 echo.  -?    Display this help page
 echo.  -a    Builds all release ^& debug versions (default)
 echo.  -d    Builds all debug versions only
-echo.  -f    Force building all program targets, irrespective of file dates
+echo.  -f^|/A Force building all program targets, irrespective of file dates
 echo.  -r    Builds all release versions only
 echo.  -X    Display the make command generated and exit
 echo.
 echo.Notes:
 echo.* exe myprog ^<==^> make myprog.exe debug\myprog.exe
-echo.* This builds all possible OS targets
-echo.* To force rebuilding all targets, irrespective of their date,
-echo.  use nmake option /A. So: exe -f myprog ^<==^> exe myprog /A
-goto :eof
+echo.* By default, it builds all possible OS targets in DOS, WIN95, WIN32, WIN64
+echo.* Macro definitions must be quoted. Ex: "OS=DOS" "MEM=L"
+exit /b
 
-:release
-%EXEC% make %MAKEOPTS% %PROGRAM%
-goto :eof
-
-:debug
-%EXEC% make %MAKEOPTS% debug\%PROGRAM%
-goto :eof
-
-:default
-:all
-%EXEC% make %MAKEOPTS% %PROGRAM% debug\%PROGRAM%
-goto :eof
+:start
+if not defined TARGETS (>&2 echo Error: No target program specified) & exit /b 1
+set CMDLINE=make%MAKEOPTS%%TARGETS%
+:# The endlocal below deletes all local variables, which sometimes did confuse make.bat.
+endlocal & %EXEC% %CMDLINE%
