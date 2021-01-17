3208ac59d66f37af0dd6479b8a9ba2d2e5dd4901
JFLarvoire <jf.larvoire@free.fr>
2021-01-17 12:01:56 +0100

    A Posix Shell initialization script defining an mcd function, doing the same as Batch/mcd.bat.

---
+++ Bash/profile.d/mcd.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+###############################################################################
+#                                                                             #
+#   Filename        mcd.sh                                                    #
+#                                                                             #
+#   Description     Define a routine that creates a directory, and enters it. #
+#                                                                             #
+#   Note            To have the routine available in every shell, put this    #
+#                   script in /etc/profile.d/.                                #
+#                                                                             #
+#   History                                                                   #
+#    2021-01-16 JFL Created this script.                                      #
+#                                                                             #
+#         © Copyright 2021 Hewlett Packard Enterprise Development LP          #
+# Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 #
+###############################################################################
+
+mcd() {
+  mkdir -p "$1" && cd "$1"
+}
