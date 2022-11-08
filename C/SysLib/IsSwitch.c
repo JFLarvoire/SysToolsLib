7a2415c04c792c91707fd58a99122937a99952c9
Jean-François Larvoire <jf.larvoire@hpe.com>
2022-11-08 17:08:44 +0100

    Should have been part of the 2022-10-19 commit!

---
+++ C/SysLib/IsSwitch.c
@@ -0,0 +1,48 @@
+﻿/*****************************************************************************\
+*									      *
+*   File name	    IsSwitch.c						      *
+*									      *
+*   Description     Test if a command line argument is a switch.	      *
+*									      *
+*   Notes								      *
+*									      *
+*   History								      *
+*    1998-05-25 JFL Created this file in BiosLib.			      *
+*    2022-10-19 JFL Moved to SysLib, with support for non-Microsoft OSs.      *
+*									      *
+*      (c) Copyright 1997-2017 Hewlett Packard Enterprise Development LP      *
+* Licensed under the Apache 2.0 license - www.apache.org/licenses/LICENSE-2.0 *
+\*****************************************************************************/
+
+#include "mainutil.h"
+
+/*---------------------------------------------------------------------------*\
+*                                                                             *
+|   Function	    IsSwitch						      |
+|									      |
+|   Description     Test if a command line argument is a switch.	      |
+|									      |
+|   Parameters      char *pszArg					      |
+|									      |
+|   Returns	    TRUE or FALSE					      |
+|									      |
+|   Notes								      |
+|									      |
+|   History								      |
+|    1997-03-04 JFL Created this routine				      |
+|    2016-08-25 JFL "-" alone is NOT a switch.				      |
+*									      *
+\*---------------------------------------------------------------------------*/
+
+int IsSwitch(char *pszArg) {
+  switch (*pszArg) {
+    case '-': /* All operating systems, including Unix, MacOS, FreeBSD, ... */
+#if defined(_WIN32) || defined(_MSDOS)
+    case '/': /* Microsoft operating systems only */
+#endif
+      return (*(short*)pszArg != (short)'-'); /* "-" is NOT a switch */
+    default:
+      return FALSE;
+  }
+}
+
