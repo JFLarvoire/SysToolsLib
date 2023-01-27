f099e5e552edfc361bd24c3f616e4977769e857e
Jean-François Larvoire <jf.larvoire@hpe.com>
2023-01-27 19:49:04 +0100

    Use a simpler alignment method, which now works well and fast in all Unix versions. Improved the system locale detection in Unix, and avoid displaying non-existent characters by default. Display more consistent and helpful verbose messages. Display debug messages in the debug version only.

--- C/SRC/chars.c
+++ C/SRC/chars.c
@@ -34,6 +34,12 @@
 *		    This allows removing the Windows Terminal detection code. *
 *		    Detect failures to get the cursor coordinates. This may   *
 *		    happen in Unix, for example in FreeBSD.		      *
+*    2023-01-27 JFL Use a simpler alignment method, which now works well and  *
+*		    fast in all Unix versions.				      *
+*		    Improved the system locale detection in Unix, and avoid   *
+*		    displaying non-existent characters by default.	      *
+*		    Display more consistent and helpful verbose messages.     *
+*		    Display debug messages in the debug version only.         *
 *		    Version 1.7.					      *
 *		    							      *
 *       © Copyright 2016-2017 Hewlett Packard Enterprise Development LP       *
@@ -43,7 +49,7 @@
 #define PROGRAM_DESCRIPTION "Output character tables"
 #define PROGRAM_NAME    "chars"
 #define PROGRAM_VERSION "1.7"
-#define PROGRAM_DATE    "2023-01-26"
+#define PROGRAM_DATE    "2023-01-27"
 
 #define _CRT_SECURE_NO_WARNINGS 1 /* Avoid Visual C++ 2005 security warnings */
 
@@ -66,6 +72,8 @@
 #define EOL "\r\n"
 
 #define SUPPORTS_UTF8 TRUE
+#define EXTRA_CHARS_IN_CONTROL_CODES TRUE
+#define ANSI_IS_OPTIONAL TRUE	/* The old console is non-ANSI; The new terminal is ANSI */
 
 #endif /* _WIN32 */
 
@@ -81,6 +89,13 @@
 #define EOL "\r\n"
 
 #define SUPPORTS_UTF8 FALSE
+#define EXTRA_CHARS_IN_CONTROL_CODES TRUE
+
+#ifdef _BIOS
+#define ANSI_IS_OPTIONAL FALSE	/* The BIOS never processes ANSI escape sequences */
+#else
+#define ANSI_IS_OPTIONAL TRUE	/* In DOS this depends on the use of ANSI.SYS */
+#endif
 
 #endif /* _MSDOS */
 
@@ -91,6 +106,8 @@
 #define EOL "\r\n"
 
 #define SUPPORTS_UTF8 FALSE
+#define EXTRA_CHARS_IN_CONTROL_CODES TRUE
+#define ANSI_IS_OPTIONAL TRUE	/* Assuming it's like in DOS */
 
 #endif /* _OS2 */
 
@@ -100,9 +117,13 @@
 
 #define _UNIX
 
+#include <locale.h>
+
 #define EOL "\n"
 
 #define SUPPORTS_UTF8 TRUE
+#define EXTRA_CHARS_IN_CONTROL_CODES FALSE
+#define ANSI_IS_OPTIONAL FALSE	/* Posix requires ANSI escape sequences support */
 
 #endif /* __unix__ */
 
@@ -128,6 +149,8 @@
 #include "mainutil.h"	/* SysLib helper routines for main() */
 #include "stversion.h"	/* SysToolsLib version strings. Include last. */
 
+DEBUG_GLOBALS			/* Define global variables used by our debugging macros */
+
 char spaces[] = "            ";
 
 void usage(void);
@@ -135,6 +158,7 @@ void usage(void);
 int ToUtf8(unsigned int c, char *b);
 #endif /* SUPPORTS_UTF8 */
 
+int Print(const char *pszString) { return printf("%s", pszString); } /* Output a string without a new line */
 int Puts(const char *pszString) { return fputs(pszString, stdout); } /* Output a string without a new line */
 #define PUTL(pszString) do { Puts(pszString); Puts(EOL); } while (0) /* Output a string with a new line in raw mode (LF untranslated) */
 
@@ -159,10 +183,13 @@ int CDECL main(int argc, char *argv[]) {
 #ifdef _WIN32
   unsigned uCP = 0;
   unsigned uCP0 = GetConsoleOutputCP();
+  unsigned uCP1 = uCP0;
+  CPINFOEX cpi = {0};
 #endif
   int iAll = FALSE;
   int iFirst = 0;
-  int iLast = 0xFF;
+  int iLast = -1;
+  int iMaxLast = 0;
   int iVerbose = FALSE;
   int iBase;
   int nBlock = 0;
@@ -170,29 +197,20 @@ int CDECL main(int argc, char *argv[]) {
   int isUTF8 = FALSE;
 #endif /* SUPPORTS_UTF8 */
 #ifdef _UNIX
-  char *pszLang = getenv("LANG");
+  char *pszLocale = setlocale(LC_ALL, "");
 #endif
 #ifndef _BIOS
   int isTTY = isatty(1);
 #else
   int isTTY = TRUE; /* The BIOS version can only write to the console */
 #endif
+#if ANSI_IS_OPTIONAL
   int isANSI = FALSE; /* ANSI terminals interpret the ESC character */
   int iErr;
-
-#if SUPPORTS_UTF8
-#ifdef _WIN32
-  if (uCP0 == 65001) isUTF8 = TRUE;
 #endif
-#ifdef _UNIX
-  if (pszLang && strstr(pszLang, "UTF-8")) isUTF8 = TRUE;
-  /* Note that Unix XTerm considers bytes \x80-\x9F as control sequences
-     equivalent to ESC @, ESC A, ESC B, ..., ESC _ . 
-     Do not output them, else there may be unpredictable effects on the console,
-     depending on what follows. */
-#endif
-  if (isUTF8) iLast = 0x7F;
-#endif /* SUPPORTS_UTF8 */
+  int isMBCS = FALSE;
+  int isASCII = FALSE;
+  int iExitCode = 1;
 
   for (i=1; i<argc; i++) {
     char *arg = argv[i];
@@ -203,6 +221,13 @@ int CDECL main(int argc, char *argv[]) {
 	iAll = TRUE;
 	continue;
       }
+#ifdef _DEBUG
+      if (streq(opt, "d")) {
+	DEBUG_ON();
+	iVerbose = TRUE;
+	continue;
+      }
+#endif
       if (   streq(opt, "h")	    /* Display usage */
 	  || streq(opt, "help")	/* The historical name of that switch */
 	  || streq(opt, "-help")
@@ -219,7 +244,7 @@ int CDECL main(int argc, char *argv[]) {
 	  uCP = 65001;
 #endif /* defined(_WIN32) */
 	  iLast = iFirst;
-	  if (iVerbose) printf("Code point 0x%X", iFirst);
+	  DEBUG_PRINTF(("Code point 0x%X", iFirst));
 	} else {
 	  fprintf(stderr, "No or bad unicode code point.\n");
 	  return 1;
@@ -227,9 +252,9 @@ int CDECL main(int argc, char *argv[]) {
 	arg = argv[i];
 	if ((arg[nc] == '-') && sscanf(arg+nc+1, "%x", &iLast)) {
 	  if (iLast < iFirst) iLast = iFirst;
-	  if (iVerbose) printf(" until 0x%X", iLast);
+	  DEBUG_PRINTF((" until 0x%X", iLast));
 	}
-	if (iVerbose) printf("\n");
+	DEBUG_PRINTF(("\n"));
 	continue;
       }
 #endif /* SUPPORTS_UTF8 */
@@ -259,6 +284,35 @@ int CDECL main(int argc, char *argv[]) {
     fprintf(stderr, "Unrecognized argument %s. Ignored.\n", arg);
   }
 
+#ifdef _UNIX
+  if (iVerbose) printf("The system locale is %s\n", pszLocale);
+#endif /* _UNIX */
+
+#if ANSI_IS_OPTIONAL
+  /* Check if the console handles ANSI escape sequences */
+  if (isTTY) {
+    int iCol, iCol0;
+    PUTC('\r'); /* Return to the first column (0 or 1 depending on systems) */
+    fflush(stdout);
+    iErr = GetCursorPosition(&iCol0, NULL);
+    if (iErr) {
+failed_to_get_cursor_coord:
+      fprintf(stderr, "Failed to get the cursor coordinates\n");
+      goto cleanup;
+    }
+    Puts(" \x1B[1D"); /* Output a space, then move the cursor back left */
+    fflush(stdout);
+    iErr = GetCursorPosition(&iCol, NULL);
+    if (iErr) goto failed_to_get_cursor_coord;
+    /* If it's an ANSI terminal, the cursor will be back on the initial column;
+       Else a string like " ?[1D" will be displayed, with the cursor on column 4 or 5 */
+    isANSI = (iCol == iCol0);
+    DEBUG_PRINTF(("\niCol = %d\n", iCol));
+    if (!isANSI) Puts("\r     \r"); /* Erase the garbage displayed on a non-ANSI terminal */
+    if (iVerbose) printf("This %s an ANSI terminal\n", isANSI ? "is" : "isn't");
+  }
+#endif /* ANSI_IS_OPTIONAL */
+
 #ifdef _WIN32
   if (uCP && (uCP != uCP0)) {
     if (iVerbose) printf("Switching to code page %d.\n", uCP);
@@ -266,15 +320,75 @@ int CDECL main(int argc, char *argv[]) {
       fprintf(stderr, "Failed to switch to code page %d.\n", uCP);
       return 1;
     }
-  } else if (iVerbose) printf("Active code page: %d\n", uCP0);
+    uCP1 = uCP;
+  } else {
+    if (iVerbose) printf("Active code page: %d\n", uCP0);
+  }
+/****** Now on, All exits must be done via the cleanup label in the end ******/
+  if (!GetCPInfoEx(uCP1, 0, &cpi)) {
+    fprintf(stderr, "Error: Unknown Code Page %d.\n", uCP);
+    goto cleanup;
+  }
+  /* Check if MSBCS code set */
+  isMBCS = (cpi.MaxCharSize > 1);
 #endif /* defined(_WIN32) */
+
+#if SUPPORTS_UTF8
+#ifdef _WIN32
+  if (uCP1 == 65001) isUTF8 = TRUE;
+  if (uCP1 == 20127) isASCII = TRUE;
+#endif
+#ifdef _UNIX
+  if (pszLocale) {
+    if (strstr(pszLocale, "UTF-8")) isUTF8 = TRUE;
+  /* Note that Unix XTerm considers bytes \x80-\x9F as control sequences
+     equivalent to ESC @, ESC A, ESC B, ..., ESC _ .
+     Do not output them, else there may be unpredictable effects on the console,
+     depending on what follows. */
+    if (   streq(pszLocale, "C")
+        || streq(pszLocale, "POSIX")
+        || !strncmp(pszLocale, "C.", 2)
+        || !strncmp(pszLocale, "C/", 2)) isASCII = TRUE;
+  }
+#endif
+  if (isUTF8) isMBCS = TRUE;
+#endif /* SUPPORTS_UTF8 */
+
+#if SUPPORTS_UTF8
+  if (isUTF8) {
+    iMaxLast = 0x10FFFF;
+    if (iVerbose) printf("This is 21-bits Unicode (0x00-0x10FFFF)\n");
+  } else
+#endif /* SUPPORTS_UTF8 */
+#ifndef _MSDOS /* TODO: Detect MBCS locales in DOS */ 
+  if (isMBCS) {
+    iMaxLast = 0xFFFF; /* TODO: Can we detect the actual maximum value? */
+    if (iVerbose) printf("This is a Multi-Byte Character Set\n");
+  } else
+#endif /* _MSDOS */
+  if (!isASCII) {
+    iMaxLast = 0xFF;
+    if (iVerbose) printf("This is an 8-bits character set (0x00-0xFF)\n");
+  }
+#ifndef _MSDOS /* TODO: Is there an ASCII locale in DOS? */ 
+  else {
+    iMaxLast = 0x7F;
+    if (iVerbose) printf("This is 7-bits ASCII (0x00-0x7F)\n");
+  }
+#endif /* _MSDOS */
+  /* Default to the whole set for SBCSs, and to the first 128 characters for smaller or larger sets */
+  if (iLast == -1) iLast = (iMaxLast == 0xFF) ? 0xFF : 0x7F;
+  /* Warn the user if his requested is likely to fail to display anything */
+  if (iLast > iMaxLast) {
+    fprintf(stderr, "Warning: The last requested char. %02X is larger than the last possible one %02X\n", iLast, iMaxLast);
+  }
 
 #if defined(_WIN32) || (defined(_MSDOS) && !(defined(_BIOS) || defined(_LODOS)))
   fflush(stdout); /* Make sure any previous output is done in text mode */
   _setmode(1, _O_BINARY ); /* fileno(stdout) = 1 */
 #endif
 
-  /* Now on, all output must use EOL instead of \n */
+/*************** Now on, all output must use EOL instead of \n ***************/
 
 #if SUPPORTS_UTF8
   if (iFirst == iLast) {
@@ -283,7 +397,7 @@ int CDECL main(int argc, char *argv[]) {
     buf[n] = '\0';
     if (!n) {
       fprintf(stderr, "Invalid code point: 0x%X." EOL, iFirst);
-      return 1;
+      goto cleanup;
     }
     if (iVerbose) {
       Puts("UTF-8 ");
@@ -295,35 +409,8 @@ int CDECL main(int argc, char *argv[]) {
   }
 #endif /* SUPPORTS_UTF8 */
 
-  /* Check if the console handles ANSI escape sequences */
-#if !defined(_BIOSSS)
-  if (isTTY) {
-    int iCol, iCol0;
-    PUTC('\r'); /* Return to the first column (0 or 1 depending on systems) */
-    fflush(stdout);
-    iErr = GetCursorPosition(&iCol0, NULL);
-    if (iErr) {
-failed_to_get_cursor_coord:
-      fprintf(stderr, "Failed to get the cursor coordinates" EOL);
-      return 1;
-    }
-    Puts(" \x1B[1D"); /* Output a space, then move the cursor back left */
-    fflush(stdout);
-    iErr = GetCursorPosition(&iCol, NULL);
-    if (iErr) goto failed_to_get_cursor_coord;
-    /* If it's an ANSI terminal, the cursor will be back on the initial column;
-       Else a string like " ?[1D" will be displayed, with the cursor on column 4 or 5 */
-    isANSI = (iCol == iCol0);
-#ifdef _DEBUG
-    printf(EOL "iCol = %d" EOL, iCol);
-#else
-    if (!isANSI) Puts("\r     \r"); /* Erase the garbage displayed on a non-ANSI terminal */
-#endif
-    if (iVerbose) printf("This %s an ANSI terminal" EOL, isANSI ? "is" : "isn't");
-  }
-#endif
-
   for (iBase = (iFirst & -0x80); iBase < ((iLast + 0x7F) & -0x80); iBase += 0x80) {
+    int iCol = 0;
     int iDigits = 2;
     for (i=0x100; i; i<<=4) if (iBase >= i) iDigits += 1;
     if (nBlock) Puts(EOL);
@@ -333,13 +420,14 @@ failed_to_get_cursor_coord:
     for (j=0; j<16; j++) {
       for (i=0; i<8; i++) {
 	int k, l;
+#if EXTRA_CHARS_IN_CONTROL_CODES
 	int iRow0, iCol0, iRow1, iCol1;
-
-	if (!(i&3)) Puts("  ");
+#endif
+	if (!(i&3)) iCol += Print("  ");
 
 	l = k = (iBase + 16*i)+j;
 	if ((k < iFirst) || (k > iLast)) {
-	  printf("%.*s", iDigits+4, spaces);
+	  iCol += printf("%.*s", iDigits+4, spaces);
 	  continue;
 	}
 	if (!iAll) switch (k) {
@@ -359,7 +447,7 @@ failed_to_get_cursor_coord:
 	      break;
 	    }
 #endif
-#if (!defined(_UNIX)) && (!defined(_BIOSSS)) /* DOS and Windows ANSI terminals choke on these control codes */
+#if ANSI_IS_OPTIONAL /* DOS and Windows ANSI terminals choke on these control codes */
 	    if (isANSI) {
 	      switch (k) {
 		case 0x00:	/* Null */
@@ -369,27 +457,37 @@ failed_to_get_cursor_coord:
 		  break;
 	      }
 	    }
-#endif
+#endif /* ANSI_IS_OPTIONAL */
+#if SUPPORTS_UTF8
+	    if (isUTF8 & (k >= 0x80) && (k <= 0x9F)) {
+	      l = ' '; /* \u80-\u9F are additional control characters */
+	      break;
+	    }
+#endif /* SUPPORTS_UTF8 */
 	    break;
 	}
 
-	printf("  %02X ", k); /* Do not use %c for the char, else the NUL char is not written */
+	iCol += printf("  %02X ", k); /* Print the numeric code, with at least 2 characters */
+#if EXTRA_CHARS_IN_CONTROL_CODES
         iRow0 = iCol0 = 0; /* Prevent an (incorrect) uninitialized variable warning later on */
-	if (isTTY && ((l < ' ') || (l >= '\x7F'))) {
+	if (isTTY && (l < ' ')) { /* Old DOS and Windows consoles display extra characters in the control codes range */
 	  fflush(stdout);
 	  iErr = GetCursorPosition(&iCol0, &iRow0);
 	  if (iErr) goto failed_to_get_cursor_coord;
 	}
+#endif /* EXTRA_CHARS_IN_CONTROL_CODES */
 #if SUPPORTS_UTF8
-	if ((k > 0x7F) && isUTF8) {
+	if ((l > 0x7F) && isUTF8) {
 	  char buf[5];
-	  int n = ToUtf8(k, buf);
+	  int n = ToUtf8(l, buf);
 	  buf[n] = '\0';
 	  Puts(buf);
 	} else
 #endif /* SUPPORTS_UTF8 */
 	PUTC(l); /* Do not use printf %c for the char, else the NUL char is not written */
-	if (isTTY && ((l < ' ') || (l >= '\x7F'))) { /* The cursor may not move for undefined or 0-width characters */
+	iCol += 1; /* Theoretically, the cursor should move 1 column forward, but there are exceptions */
+#if EXTRA_CHARS_IN_CONTROL_CODES
+	if (isTTY && (l < ' ')) { /* The cursor may make wild moves for some control codes */
 	  fflush(stdout);
 	  iErr = GetCursorPosition(&iCol1, &iRow1);
 	  if (iErr) goto failed_to_get_cursor_coord;
@@ -401,15 +499,26 @@ failed_to_get_cursor_coord:
 	    SetCursorPosition(iCol0+1, iRow0-1);
 	  }
 	}
+#endif /* EXTRA_CHARS_IN_CONTROL_CODES */
+	if (isTTY && (l >= '\x7F')) { /* The cursor may not move for undefined or 0-width characters */
+#ifdef _UNIX
+	  printf("\x1B[%dG", iCol+1); /* Go to absolute column iCol (1-based in ANSI coordinates) */
+#else /* DOS, Windows */
+	  fflush(stdout);
+	  GetCursorPosition(&iCol1, &iRow1);
+	  if (iCol1 != iCol) SetCursorPosition(iCol, iRow1);
+#endif
+	}
       }
       Puts(EOL);
+      iCol = 0;
       nBlock += 1;
     }
   }
 
-#if SUPPORTS_UTF8
+  iExitCode = 0;
+
 cleanup:
-#endif /* SUPPORTS_UTF8 */
 #ifdef _WIN32
   if (uCP && (uCP != uCP0)) {
     if (iVerbose) printf("Switching back to code page %d." EOL, uCP0);
@@ -420,7 +529,7 @@ cleanup:
   }
 #endif
 
-  return 0;
+  return iExitCode;
 }
 
 void usage(void) {
@@ -441,6 +550,10 @@ PROGRAM_NAME_AND_VERSION " - " PROGRAM_DESCRIPTION "\n\
 Switches:\n\
   -?|-h|--help        Display this help screen\n\
   -a|--all            Output all characters, even control chars like CR LF, etc\n"
+#ifdef _DEBUG
+"\
+  -d|--debug          Display debug information\n"
+#endif
 #if SUPPORTS_UTF8
 "\
   -u|--unicode X[-Y]  Display a Unicode character, or a range of characters\n"
@@ -479,4 +592,3 @@ int ToUtf8(unsigned int c, char *b) {
   return (int)(b-b0);
 }
 #endif /* SUPPORTS_UTF8 */
-
